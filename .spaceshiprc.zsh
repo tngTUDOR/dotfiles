a2ffcfdeb1a6cb93bf6850b048ed85601f7d17f0
tngTUDOR <tngTUDOR@users.noreply.github.com>
2025-06-05 13:50:20 +0200

    add spaceship zsh theme

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,44 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Add custom Ember section
+# See: https://github.com/spaceship-prompt/spaceship-ember
+#spaceship add ember
+
+# Add a custom vi-mode section to the prompt
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+#spaceship add --before char vi_mode
+
+SPACESHIP_VENV_GENERIC_NAMES=(virtualenv venv .venv generic-name)
+#SPACESHIP_VENV_ASYNC=true
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_GIT_BRANCH_SHOW_COMMIT=true
+SPACESHIP_GIT_BRANCH_SHOW_TAG=true
+SPACESHIP_VENV_SHOW=true
+#SPACESHIP_PROMPT_ORDER=(
+  #time          # Time stamps section
+  #user          # Username section
+  #dir           # Current directory section
+  #host          # Hostname section
+  #git           # Git section (git_branch + git_status)
+  ##node          # Node.js section
+  ##golang        # Go section
+  ##julia         # Julia section
+  #docker        # Docker section
+  #conda         # conda virtualenv section
+  ##kubecontext   # Kubectl context section
+  #line_sep      # Line break
+  #exec_time     # Execution time
+  ##battery       # Battery level and status
+  #vi_mode       # Vi-mode indicator
+  #jobs          # Background jobs indicator
+  #exit_code     # Exit code section
+  #char          # Prompt character
+#)
+
